1944832554ea3aa93a70815fe75e2d7bacbaad8c
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-15 12:45:26 -0300

    fix: adds missing parameter to localstack script

--- localstack/010-dynamodb.sh
+++ localstack/010-dynamodb.sh
@@ -5,4 +5,5 @@ set -v
 awslocal dynamodb create-table \
   --table-name Pagamentos \
   --attribute-definitions '[ { "AttributeName": "id", "AttributeType": "S" }]' \
-  --key-schema '[ { "AttributeName": "id", "KeyType": "HASH" }]'
+  --key-schema '[ { "AttributeName": "id", "KeyType": "HASH" }]' \
+  --billing-mode PAY_PER_REQUEST
